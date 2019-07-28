05febb565d9b521768a5a55236b77c0913fb7ec6
Mark Nagel <mark_nagel@hotmail.com>
2019-07-27 19:11:45 -0500

    mid problem solve - kids need attention

---
+++ src/practice.js
@@ -0,0 +1,40 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block in a direction and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+// Test.expect(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
+// Test.expect(!isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
+// Test.expect(!isValidWalk(['w']), 'should return false');
+// Test.expect(!isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
+
+
+let arr = ['n','s','n','s','n','s','n','s','n','s']
+console.log(arr.length)
+
+
+function checklength(arr){
+  let nCount = 0;
+  console.log(nCount)
+  let sCount = 0;
+  console.log(sCount)
+  let eCount = 0;
+  console.log(eCount)
+  let wCount = 0;
+  console.log(wCount)
+
+   if(arr.length !== 10){
+     return(false)
+   }
+   else{
+
+
+      // for(var i=0; i < arr.length; i++) { 
+      //   if(i == 'n') {
+      //   nCount++; 
+      // }
+
+     return(true)
+   }
+}
+
+checklength(arr);
